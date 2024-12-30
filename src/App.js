cc25489adaa3b9429ac4caaf092e0258257104a9
DilkiAttanayake <wathsalaattanayake002@gmail.com>
2024-12-30 22:20:43 +0530

    chore(app): update app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react'; // import the React library to use JSX and React features
-import Nav from './Components/Nav'; // import the navigation bar component
-import Footer from './Components/Footer'; // import the footer component
+import React from 'react';
+import Nav from './components/Nav'; // import the navigation bar component
+import Footer from './components/Footer'; // import the footer component
+import Search from './components/Search';
+import Items from './components/Items'; // import the Items component to display properties
+
 
 // the main app component of the React application
 function App() {
@@ -8,7 +11,13 @@ function App() {
     <div className="App">
       {/* Render the navigation bar */}
       <Nav />
-      
+
+      {/* Render the Search component */}
+      <Search/>
+
+      {/* Render the Items component to display the list of properties */}
+      <Items />
+
       {/* Render the footer */}
       <Footer />
     </div>
@@ -17,5 +26,3 @@ function App() {
 
 // export the App component so it can be used in other parts of the application
 export default App;
-
-// This is the main component that renders the Nav and Footer components.
