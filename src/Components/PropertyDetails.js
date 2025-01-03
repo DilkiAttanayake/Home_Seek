096e60b80eed84154a9d6d14c17c23711510500a
DilkiAttanayake <wathsalaattanayake002@gmail.com>
2025-01-03 16:19:24 +0530

    feat(PropertyDetails): sanitize property location input to prevent XSS vulnerabilities

--- src/Components/PropertyDetails.js
+++ src/Components/PropertyDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import DOMPurify from 'dompurify'; // Import DOMPurify for sanitizing HTML
 
 const PropertyDetails = ({ property }) => {
   return (
@@ -21,7 +22,8 @@ const PropertyDetails = ({ property }) => {
       <TabPanel>
         <h4 style={{color:'#575e62'}}>Google Map</h4>
         <iframe
-          src={`https://www.google.com/maps?q=${property.location}&output=embed`}
+          
+          src={`https://www.google.com/maps?q=${DOMPurify.sanitize(property.location)}&output=embed`}
           width="100%"
           height="450"
           style={{ border: 0 }}
