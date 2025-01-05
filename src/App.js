9b7be55e6301bd091951ed4695515ae679722d04
DilkiAttanayake <wathsalaattanayake002@gmail.com>
2025-01-05 23:16:27 +0530

    feat(Routes): update comments for property page navigation and form routes

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Properties />} /> {/* Route for properties listing */}
           <Route path="/property/:propertyId" element={<PropertyPage />} /> {/* Route for individual property page */}
-          <Route path="/nav/:propertyId" element={<PropertyPage />} /> {/* Route for property page */}
-          <Route path="/form/:propertyId" element={<PropertyPage />} /> {/* Route for property page */}
+          <Route path="/nav/:propertyId" element={<PropertyPage />} /> {/* Route for property page form nav search */}
+          <Route path="/form/:propertyId" element={<PropertyPage />} /> {/* Route for property page from form search*/}
         </Routes>
 
         <Footer /> {/* Footer component */}
